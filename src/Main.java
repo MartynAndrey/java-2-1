18c886cb57da8207f5ae50f56e3742e7d42fd2e8
Andrey Martyn <martynandrey@rambler.ru>
2021-11-06 12:09:23 +0600

    Application is implemented in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        double ticketPrice = 485.9;
+        double bonusMileCost = 15;
 
+        if (ticketPrice <= 0) {
+            System.out.println("The price of ticket cannot be less than zero.");
+            return;
+        }
+        if (bonusMileCost <= 0) {
+            System.out.println("The cost of one bonus mile cannot be less than zero.");
+            return;
+        }
+
+        int bonusMileCount = (int)(ticketPrice / bonusMileCost);
+        System.out.println("The count of bonus miles = " + bonusMileCount);
     }
 }
